a8ac65c93dfcf613dbfffa18c79b19c74daa38d0
David DeSandro <desandrocodes@gmail.com>
2014-12-15 21:30:59 -0500

    cells

--- src/cell.js
+++ src/cell.js
@@ -1,8 +1,15 @@
 ( function( window ) {
 
 function Cell( elem, parent ) {
+  this.element = elem;
+  this.parent = parent;
 
+  this.create();
 }
+
+Cell.prototype.create = function() {
+  this.element.style.position = 'absolute';
+};
 
 window.Cell = Cell;
 
--- src/flickity.js
+++ src/flickity.js
@@ -36,8 +36,26 @@ Flickity.defaults = {
 Flickity.prototype = new EventEmitter();
 
 Flickity.prototype._create = function() {
+
+  this.element.style.position = 'relative';
+
+
+  // slider element does all the positioning
+  this.slider = document.createElement('div');
+  this.slider.className = 'flickity-slider';
+  this.slider.style.position = 'absolute';
+  this.slider.style.width = '100%';
+
+  // wrap child elements in slider
+  while ( this.element.children.length ) {
+    this.slider.appendChild( this.element.children[0] )
+  }
+  this.element.appendChild( this.slider );
+
+
   // get cells from children
   this.reloadCells();
+
 };
 
 /**
@@ -51,7 +69,7 @@ Flickity.prototype.option = function( opts ) {
 // goes through all children
 Flickity.prototype.reloadCells = function() {
   // collection of item elements
-  this.cells = this._makeCells( this.element.children );
+  this.cells = this._makeCells( this.slider.children );
 };
 
 /**
