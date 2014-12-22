25239bb4307f674df5e6fbfeca3e1499957e9abf
David DeSandro <desandrocodes@gmail.com>
2014-12-22 14:17:11 -0500

    add rightToLeft support

--- js/cell.js
+++ js/cell.js
@@ -24,7 +24,8 @@ Cell.prototype.setPosition = function( x ) {
   this.x = x;
   this.target = x + this.size.width * this.parent.options.targetPosition;
   // render position of cell with in slider
-  this.element.style.left = this.parent.getPositionValue( this.x );
+  var side = this.parent.options.rightToLeft ? 'right' : 'left';
+  this.element.style[ side ] = this.parent.getPositionValue( this.x );
 };
 
 window.Cell = Cell;
