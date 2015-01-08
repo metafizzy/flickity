21a8678f8a5bc1df6ab2616f67de70f7f4ad7373
David DeSandro <desandrocodes@gmail.com>
2015-01-08 09:50:53 -0500

    move cellChange prototype into cell-change.js

---
+++ js/cell-change.js
@@ -0,0 +1,144 @@
+( function( window ) {
+
+'use strict';
+
+var U = window.utils;
+
+// append cells to a document fragment
+function getCellsFragment( cells ) {
+  var fragment = document.createDocumentFragment();
+  for ( var i=0, len = cells.length; i < len; i++ ) {
+    var cell = cells[i];
+    fragment.appendChild( cell.element );
+  }
+  return fragment;
+}
+
+// -------------------------- cell change prototype -------------------------- //
+
+var proto = {};
+
+/**
+ * Insert, prepend, or append cells
+ * @param {Element, Array, NodeList} elems
+ * @param {Integer} index
+ */
+proto.insert = function( elems, index ) {
+  var cells = this._makeCells( elems );
+  if ( !cells || !cells.length ) {
+    return;
+  }
+  var len = this.cells.length;
+  // default to append
+  index = index === undefined ? len : index;
+  // add cells with document fragment
+  var fragment = getCellsFragment( cells );
+  // append to slider
+  var isAppend = index === len;
+  if ( isAppend ) {
+    this.slider.appendChild( fragment );
+  } else {
+    var insertCellElement = this.cells[ index ].element;
+    this.slider.insertBefore( fragment, insertCellElement );
+  }
+  // add to this.cells
+  if ( index === 0 ) {
+    // prepend, add to start
+    this.cells = cells.concat( this.cells );
+  } else if ( isAppend ) {
+    // append, add to end
+    this.cells = this.cells.concat( cells );
+  } else {
+    // insert in this.cells
+    var endCells = this.cells.splice( index, len - index );
+    this.cells = this.cells.concat( cells ).concat( endCells );
+  }
+
+  this._sizeCells( cells );
+  this._cellAddedRemoved( index );
+};
+
+proto.append = function( elems ) {
+  this.insert( elems, this.cells.length );
+};
+
+proto.prepend = function( elems ) {
+  this.insert( elems, 0 );
+};
+
+/**
+ * Remove cells
+ * @param {Element, Array, NodeList} elems
+ */
+proto.remove = function( elems ) {
+  var cells = this.getCells( elems );
+  for ( var i=0, len = cells.length; i < len; i++ ) {
+    var cell = cells[i];
+    cell.remove();
+    // remove item from collection
+    U.removeFrom( cell, this.cells );
+  }
+
+  if ( cells.length ) {
+    // update stuff
+    this._cellAddedRemoved( 0 );
+  }
+};
+
+// updates when cells are added or removed
+proto._cellAddedRemoved = function( index ) {
+  // update page dots
+  if ( this.pageDots ) {
+    this.pageDots.setDots();
+  }
+  // TODO cell is removed before the selected cell, adjust selectedIndex by -1
+  this.selectedIndex = Math.max( 0, Math.min( this.cells.length - 1, this.selectedIndex ) );
+
+  this.cellChange( index );
+};
+
+/**
+ * logic to be run after a cell's size changes
+ * @param {Element} elem - cell's element
+ */
+proto.cellSizeChange = function( elem ) {
+  var cell = this.getCell( elem );
+  if ( !cell ) {
+    return;
+  }
+  cell.getSize();
+
+  var index = U.indexOf( this.cells, cell );
+  this.cellChange( index );
+};
+
+/**
+ * logic any time a cell is changed: added, removed, or size changed
+ * @param {Integer} index - index of the changed cell, optional
+ */
+proto.cellChange = function( index ) {
+  // TODO maybe always size all cells unless isSkippingSizing
+  // size all cells if necessary
+  // if ( !isSkippingSizing ) {
+  //   this._sizeCells( this.cells );
+  // }
+
+  index = index || 0;
+
+  this._positionCells( index );
+  this._getWrapShiftCells();
+  this.setContainerSize();
+  // position slider
+  if ( this.options.freeScroll ) {
+    this.positionSlider();
+  } else {
+    this.select( this.selectedIndex );
+  }
+};
+
+// -----  ----- //
+
+window.Flickity = window.Flickity || {};
+window.Flickity.cellChangePrototype = proto;
+
+})( window );
--- js/flickity.js
+++ js/flickity.js
@@ -14,6 +14,7 @@ var jQuery = window.jQuery;
 var U = window.utils;
 var dragPrototype = window.Flickity.dragPrototype;
 var animatePrototype = window.Flickity.animatePrototype;
+var cellChangePrototype = window.Flickity.cellChangePrototype;
 
 function moveChildren( fromElem, toElem ) {
   while ( fromElem.children.length ) {
@@ -395,7 +396,7 @@ Flickity.prototype.uiChange = function() {
   delete this.isFreeScrolling;
 };
 
-// -------------------------- add/remove -------------------------- //
+// -------------------------- get cells -------------------------- //
 
 /**
  * get Flickity.Cell, given an Element
@@ -428,134 +429,6 @@ Flickity.prototype.getCells = function( elems ) {
     }
   }
   return cells;
-};
-
-// append cells to a document fragment
-function getCellsFragment( cells ) {
-  var fragment = document.createDocumentFragment();
-  for ( var i=0, len = cells.length; i < len; i++ ) {
-    var cell = cells[i];
-    fragment.appendChild( cell.element );
-  }
-  return fragment;
-}
-
-/**
- * Insert, prepend, or append cells
- * @param {Element, Array, NodeList} elems
- * @param {Integer} index
- */
-Flickity.prototype.insert = function( elems, index ) {
-  var cells = this._makeCells( elems );
-  if ( !cells || !cells.length ) {
-    return;
-  }
-  var len = this.cells.length;
-  // default to append
-  index = index === undefined ? len : index;
-  // add cells with document fragment
-  var fragment = getCellsFragment( cells );
-  // append to slider
-  var isAppend = index === len;
-  if ( isAppend ) {
-    this.slider.appendChild( fragment );
-  } else {
-    var insertCellElement = this.cells[ index ].element;
-    this.slider.insertBefore( fragment, insertCellElement );
-  }
-  // add to this.cells
-  if ( index === 0 ) {
-    // prepend, add to start
-    this.cells = cells.concat( this.cells );
-  } else if ( isAppend ) {
-    // append, add to end
-    this.cells = this.cells.concat( cells );
-  } else {
-    // insert in this.cells
-    var endCells = this.cells.splice( index, len - index );
-    this.cells = this.cells.concat( cells ).concat( endCells );
-  }
-
-  this._sizeCells( cells );
-  this._cellAddedRemoved( index );
-};
-
-Flickity.prototype.append = function( elems ) {
-  this.insert( elems, this.cells.length );
-};
-
-Flickity.prototype.prepend = function( elems ) {
-  this.insert( elems, 0 );
-};
-
-/**
- * Remove cells
- * @param {Element, Array, NodeList} elems
- */
-Flickity.prototype.remove = function( elems ) {
-  var cells = this.getCells( elems );
-  for ( var i=0, len = cells.length; i < len; i++ ) {
-    var cell = cells[i];
-    cell.remove();
-    // remove item from collection
-    U.removeFrom( cell, this.cells );
-  }
-
-  if ( cells.length ) {
-    // update stuff
-    this._cellAddedRemoved( 0 );
-  }
-};
-
-// updates when cells are added or removed
-Flickity.prototype._cellAddedRemoved = function( index ) {
-  // update page dots
-  if ( this.pageDots ) {
-    this.pageDots.setDots();
-  }
-  // TODO cell is removed before the selected cell, adjust selectedIndex by -1
-  this.selectedIndex = Math.max( 0, Math.min( this.cells.length - 1, this.selectedIndex ) );
-
-  this.cellChange( index );
-};
-
-/**
- * logic to be run after a cell's size changes
- * @param {Element} elem - cell's element
- */
-Flickity.prototype.cellSizeChange = function( elem ) {
-  var cell = this.getCell( elem );
-  if ( !cell ) {
-    return;
-  }
-  cell.getSize();
-
-  var index = U.indexOf( this.cells, cell );
-  this.cellChange( index );
-};
-
-/**
- * logic any time a cell is changed: added, removed, or size changed
- * @param {Integer} index - index of the changed cell, optional
- */
-Flickity.prototype.cellChange = function( index ) {
-  // TODO maybe always size all cells unless isSkippingSizing
-  // size all cells if necessary
-  // if ( !isSkippingSizing ) {
-  //   this._sizeCells( this.cells );
-  // }
-
-  index = index || 0;
-
-  this._positionCells( index );
-  this._getWrapShiftCells();
-  this.setContainerSize();
-  // position slider
-  if ( this.options.freeScroll ) {
-    this.positionSlider();
-  } else {
-    this.select( this.selectedIndex );
-  }
 };
 
 // -------------------------- events -------------------------- //
@@ -689,6 +562,7 @@ Flickity.prototype.destroy = function() {
 
 U.extend( Flickity.prototype, dragPrototype );
 U.extend( Flickity.prototype, animatePrototype );
+U.extend( Flickity.prototype, cellChangePrototype );
 
 // --------------------------  -------------------------- //
 
