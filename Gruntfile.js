e7267c6760ca289ac97892ec136b48c9706eee6e
David DeSandro <desandrocodes@gmail.com>
2015-01-10 12:02:21 -0500

    add Gruntfile for building dist/

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+/*jshint node: true, strict: false */
+
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+    jshint: {
+      source: [ 'js/**/*.js'  ],
+      options: grunt.file.readJSON('.jshintrc')
+    },
+
+    concat: {
+      pkgd: {
+        dest: 'dist/flickity.pkgd.js',
+        src: [
+          // dependencies
+          'bower_components/get-style-property/get-style-property.js',
+          'bower_components/get-size/get-size.js',
+          'bower_components/matches-selector/matches-selector.js"></sc',
+          'bower_components/eventEmitter/EventEmitter.js',
+          'bower_components/eventie/eventie.js',
+          'bower_components/doc-ready/doc-ready.js"></sc',
+          'bower_components/classie/classie.js',
+          // source
+          'js/utils.js',
+          'js/unipointer.js',
+          'js/cell.js"></',
+          'js/prev-next-button.js',
+          'js/page-dots.js',
+          'js/player.js"></',
+          'js/drag.js',
+          'js/animate.js',
+          'js/cell-change.js"></',
+          'js/flickity.js'
+        ]
+      } 
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask( 'default', [
+    'jshint',
+    'concat'
+  ]);
+
+};
