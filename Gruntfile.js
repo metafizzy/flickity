e8fa112a504a689367667b76415513c82745060b
David DeSandro <desandrocodes@gmail.com>
2015-01-11 10:29:59 -0500

    typos in grunt js sources

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,21 +16,21 @@ module.exports = function( grunt ) {
           // dependencies
           'bower_components/get-style-property/get-style-property.js',
           'bower_components/get-size/get-size.js',
-          'bower_components/matches-selector/matches-selector.js"></sc',
+          'bower_components/matches-selector/matches-selector.js',
           'bower_components/eventEmitter/EventEmitter.js',
           'bower_components/eventie/eventie.js',
-          'bower_components/doc-ready/doc-ready.js"></sc',
+          'bower_components/doc-ready/doc-ready.js',
           'bower_components/classie/classie.js',
           // source
           'js/utils.js',
           'js/unipointer.js',
-          'js/cell.js"></',
+          'js/cell.js',
           'js/prev-next-button.js',
           'js/page-dots.js',
-          'js/player.js"></',
+          'js/player.js',
           'js/drag.js',
           'js/animate.js',
-          'js/cell-change.js"></',
+          'js/cell-change.js',
           'js/flickity.js'
         ]
       } 
