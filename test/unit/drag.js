70c0c55f42d4137ccb9ac798825dfc9fc7efd045
David DeSandro <desandrocodes@gmail.com>
2015-01-13 14:39:54 -0500

    add wrap-around drag test

--- test/unit/drag.js
+++ test/unit/drag.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+var utils = window.utils;
+
 function noop() {}
 
 var fakeDrag = window.fakeDrag = function( flkty, positions ) {
@@ -43,23 +45,53 @@ var fakeDrag = window.fakeDrag = function( flkty, positions ) {
   triggerEvent();
 };
 
-test( 'drag', function( assert ) {
-  // async test
-  var done = assert.async();
-
-  var flkty = new Flickity('#drag');
-
-  // do each drag test one after another
-  function doNextTest() {
+var dragTests;
+// do each drag test one after another
+function getDoNextDragTest( done ) {
+  return function doNextDragTest() {
     if ( dragTests.length ) {
       var dragTest = dragTests.shift();
       dragTest();
     } else {
       done();
     }
-  }
+  };
+}
 
-  var dragTests = [
+// flickity, dragPositions, index, callback, message
+function getFakeDragTest( args ) {
+  var assert = args.assert;
+  var flkty = args.flickity;
+  var msgCell = 'cell[' + args.index + ']';
+
+  return function fakeDragTest() {
+    var selectMsg = ( args.message ? args.message + '. ' : '' ) + 'selected ' + msgCell;
+    flkty.once( 'select', function() {
+      assert.equal( flkty.selectedIndex, args.index, selectMsg );
+    });
+
+    var settleMsg = ( args.message ? args.message + '. ' : '' ) + 'settled ' + msgCell;
+    var target = flkty.cells[ args.index ].target;
+    flkty.once( 'settle', function() {
+      assert.equal( Math.round( -flkty.x ), Math.round( target ), settleMsg );
+      args.callback();
+    });
+
+    fakeDrag( args.flickity, args.dragPositions );
+  };
+}
+
+
+
+test( 'drag', function( assert ) {
+  // async test
+  var done = assert.async();
+
+  var flkty = new Flickity('#drag');
+
+  var doNextDragTest = getDoNextDragTest( done );
+
+  dragTests = [
     // drag to 2nd cell
     function() {
       flkty.once( 'select', function() {
@@ -67,7 +99,7 @@ test( 'drag', function( assert ) {
       });
       flkty.once( 'settle', function() {
         equal( Math.round( -flkty.x ), Math.round( flkty.cells[1].target ), 'settled at 2nd cell' );
-        doNextTest();
+        doNextDragTest();
       });
       fakeDrag( flkty, [ 0, -10, -20 ] );
     },
@@ -78,7 +110,7 @@ test( 'drag', function( assert ) {
       });
       flkty.once( 'settle', function() {
         equal( Math.round( -flkty.x ), Math.round( flkty.cells[0].target ), 'settled at 1st cell' );
-        doNextTest();
+        doNextDragTest();
       });
       fakeDrag( flkty, [ 0, 10, 20 ] );
     },
@@ -89,7 +121,7 @@ test( 'drag', function( assert ) {
       });
       flkty.once( 'settle', function() {
         equal( Math.round( -flkty.x ), Math.round( flkty.cells[2].target ), 'settled at 3rd cell' );
-        doNextTest();
+        doNextDragTest();
       });
       fakeDrag( flkty, [ 0, -10, -100 ] );
     },
@@ -100,17 +132,58 @@ test( 'drag', function( assert ) {
       });
       flkty.once( 'settle', function() {
         equal( Math.round( -flkty.x ), Math.round( flkty.cells[5].target ), 'settled at last cell' );
-        doNextTest();
+        doNextDragTest();
       });
       fakeDrag( flkty, [ 0, -10, -50, -77, -100, -125, -150, -175, -250, -350 ] );
     }
   ];
 
+
+
   // trigger async
   // triggering immediately causes bug
-  setTimeout( doNextTest );
+  setTimeout( doNextDragTest );
 
 });
 
+test( 'drag with wrapAround', function( assert ) {
+  // async test
+  var done = assert.async();
+
+  var flkty = new Flickity('#drag-wrap-around', {
+    wrapAround: true
+  });
+
+  var doNextDragTest = getDoNextDragTest( done );
+
+  function getDragTest( args ) {
+    args = utils.extend( args, {
+      assert: assert,
+      flickity: flkty,
+      callback: doNextDragTest
+    });
+    return getFakeDragTest( args );
+  }
+
+  dragTests = [
+    getDragTest({
+      message: 'drag to last cell via wrap-around',
+      index: 5,
+      dragPositions: [ 0, 10, 20 ]
+    }),
+    getDragTest({
+      message: 'drag to first cell via wrap-around',
+      index: 0,
+      dragPositions: [ 0, -10, -20 ]
+    })
+  ];
+
+  // trigger async
+  setTimeout( doNextDragTest );
+
+});
+
+
 })();
+
 
