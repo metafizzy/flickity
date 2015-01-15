a6a9c321412d5a33f862a06eceb89cd9448eb16c
David DeSandro <desandrocodes@gmail.com>
2015-01-15 18:40:49 -0500

    add requirejs gulp task refactored from gulp-requirejs-optimize

--- gulpfile.js
+++ gulpfile.js
@@ -37,40 +37,71 @@ gulp.task( 'concat:js', function() {
 });
 
 
-// requirejs
-var rjs = require('gulp-r');
+// -------------------------- RequireJS makes pkgd -------------------------- //
+
+// refactored from gulp-requirejs-optimize
+// https://www.npmjs.com/package/gulp-requirejs-optimize/
+
+var gutil = require('gulp-util');
+var through = require('through2');
+var requirejs = require('requirejs');
+var chalk = require('chalk');
+
+function rjsOptimize( options ) {
+  var stream;
+
+  requirejs.define('node/print', [], function() {
+    return function(msg) {
+      if( msg.substring(0, 5) === 'Error' ) {
+        gutil.log( chalk.red( msg ) );
+      } else {
+        gutil.log( msg );
+      }
+    };
+  });
+
+  options = options || {};
+
+  stream = through.obj(function (file, enc, cb) {
+    if ( file.isNull() ) {
+      this.push( file );
+      return cb();
+    }
+
+    options.logLevel = 2;
+
+    options.out = function( text ) {
+      var outFile = new gutil.File({
+        path: file.relative,
+        contents: new Buffer( text )
+      });
+      cb( null, outFile );
+    };
+
+    gutil.log('RequireJS optimizing');
+    requirejs.optimize( options, null, function( err ) {
+      var gulpError = new gutil.PluginError( 'requirejsOptimize', err.message );
+      stream.emit( 'error', gulpError );
+    });
+  });
+
+  return stream;
+}
+
+var rename = require('gulp-rename');
 
 gulp.task( 'requirejs', function() {
-  gulp.src('js/*.js')
-    .pipe( rjs({
-      baseUrl: '../js'
+  gulp.src('js/flickity.js')
+    .pipe( rjsOptimize({
+      baseUrl: 'bower_components',
+      optimize: 'none',
+      include: [
+        'flickity/js/flickity'
+      ],
+      paths: {
+        flickity: '../'
+      }
     }) )
-  // gulp.src('')
-    // .pipe( rjs({
-    //   baseUrl: 'bower_components',
-    //   include: [
-    //     'jquery-bridget/jquery.bridget',
-    //     'flickity/js/flickity'
-    //   ],
-    //   paths: {
-    //     flickity: '../',
-    //     jquery: 'empty:'
-    //   }
-    // }) )
-    .pipe( gulp.dest('dist/rjs') );
-});
-
-
-var amdOptimize = require('amd-optimize');
-
-gulp.task( 'opt', function() {
-  amdOptimize.src( '../js/flickity', {
-    baseUrl: 'bower_components',
-    include: [
-      'jquery-bridget/jquery.bridget'
-    ],
-    preserveComments: true
-  })
-    .pipe( concat('flickity.opt.js') )
+    .pipe( rename('flickity.grjs.js') )
     .pipe( gulp.dest('dist') );
 });
