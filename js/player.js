af64545b4f528ae0a076f14bd3579b620b72f102
David DeSandro <desandrocodes@gmail.com>
2015-01-23 09:59:34 -0500

    add page visibility listening to pause/unpause autoPlay Fixes #6

--- js/player.js
+++ js/player.js
@@ -20,9 +20,30 @@
 
 'use strict';
 
+// -------------------------- Page Visibility -------------------------- //
+// https://developer.mozilla.org/en-US/docs/Web/Guide/User_experience/Using_the_Page_Visibility_API
+
+var hiddenProperty, visibilityEvent;
+if ( 'hidden' in document ) {
+  hiddenProperty = 'hidden';
+  visibilityEvent = 'visibilitychange';
+} else if ( 'webkitHidden' in document ) {
+  hiddenProperty = 'webkitHidden';
+  visibilityEvent = 'webkitvisibilitychange';
+}
+
+// -------------------------- Player -------------------------- //
+
 function Player( parent ) {
   this.isPlaying = false;
   this.parent = parent;
+  // visibility change event handler
+  if ( visibilityEvent ) {
+    var _this = this;
+    this.onVisibilityChange = function() {
+      _this.visibilityChange();
+    };
+  }
 }
 
 // start play
@@ -30,6 +51,10 @@ Player.prototype.play = function() {
   this.isPlaying = true;
   // playing kills pauses
   delete this.isPaused;
+  // listen to visibility change
+  if ( visibilityEvent ) {
+    document.addEventListener( visibilityEvent, this.onVisibilityChange, false );
+  }
   // start ticking
   this.tick();
 };
@@ -39,6 +64,8 @@ Player.prototype.tick = function() {
   if ( !this.isPlaying || this.isPaused ) {
     return;
   }
+  // keep track of when .tick()
+  this.tickTime = new Date();
   var time = this.parent.options.autoPlay;
   // default to 3 seconds
   time = typeof time == 'number' ? time : 3000;
@@ -54,6 +81,10 @@ Player.prototype.stop = function() {
   // stopping kills pauses
   delete this.isPaused;
   this.clear();
+  // remove visibility change event
+  if ( visibilityEvent ) {
+    document.removeEventListener( visibilityEvent, this.onVisibilityChange, false );
+  }
 };
 
 Player.prototype.clear = function() {
@@ -72,6 +103,12 @@ Player.prototype.unpause = function() {
   if ( this.isPaused ) {
     this.play();
   }
+};
+
+// pause if page visibility is hidden, unpause if visible
+Player.prototype.visibilityChange = function() {
+  var isHidden = document[ hiddenProperty ];
+  this[ isHidden ? 'pause' : 'unpause' ]();
 };
 
 return Player;
