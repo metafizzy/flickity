4ecc92848b2481a0f3316c34583eb9bbbf8ecd9d
David DeSandro <desandrocodes@gmail.com>
2015-02-01 11:59:19 -0500

    add sync.js

---
+++ js/sync.js
@@ -0,0 +1,132 @@
+( function( window, factory ) {
+  'use strict';
+  // universal module definition
+
+  if ( typeof define == 'function' && define.amd ) {
+    // AMD
+    define( [
+      './Flickity',
+      'fizzy-ui-utils/utils'
+    ], function( Flickity, utils ) {
+      return factory( window, Flickity, utils );
+    });
+  } else if ( typeof exports == 'object' ) {
+    // CommonJS
+    module.exports = factory(
+      window,
+      require('/.flickity'),
+      require('fizzy-ui-utils')
+    );
+  } else {
+    // browser global
+    window.Flickity = window.Flickity || {};
+    window.Flickity = factory(
+      window,
+      window.Flickity,
+      window.fizzyUIUtils
+    );
+  }
+
+}( window, function factory( window, Flickity, utils ) {
+
+'use strict';
+
+// -------------------------- sync prototype -------------------------- //
+
+// Flickity.defaults.sync = false;
+
+Flickity.createMethods.push('_createSync');
+
+Flickity.prototype._createSync = function() {
+  this.syncers = {};
+  var syncOption = this.options.sync;
+  if ( !syncOption ) {
+    return;
+  }
+  // HACK do async, give time for other flickity to be initalized
+  var _this = this;
+  setTimeout( function initSyncCompanion() {
+    _this.sync( syncOption );
+  });
+};
+
+/**
+ * sync
+ * @param {Element} or {String} elem
+ */
+Flickity.prototype.sync = function( elem ) {
+  elem = utils.getQueryElement( elem );
+  var companion = Flickity.data( elem );
+  if ( !companion ) {
+    return;
+  }
+  // two hearts, that beat as one
+  this._syncCompanion( companion );
+  companion._syncCompanion( this );
+};
+
+/**
+ * @param {Flickity} companion
+ */
+Flickity.prototype._syncCompanion = function( companion ) {
+  var _this = this;
+  function syncListener() {
+    var index = _this.selectedIndex;
+    // do not select if already selected, prevent infinite loop
+    if ( companion.selectedIndex != index ) {
+      companion.select( index );
+    }
+  }
+  this.on( 'select', syncListener );
+  // keep track of all synced flickities
+  // hold on to listener to unsync
+  this.syncers[ companion.guid ] = {
+    flickity: companion,
+    listener: syncListener
+  };
+};
+
+/**
+ * unsync
+ * @param {Element} or {String} elem
+ */
+Flickity.prototype.unsync = function( elem ) {
+  elem = utils.getQueryElement( elem );
+  var companion = Flickity.data( elem );
+  this._unsync( companion );
+};
+
+/**
+ * @param {Flickity} companion
+ */
+Flickity.prototype._unsync = function( companion ) {
+  if ( !companion ) {
+    return;
+  }
+  // I love you but I've chosen darkness
+  this._unsyncCompanion( companion );
+  companion._unsyncCompanion( this );
+};
+
+/**
+ * @param {Flickity} companion
+ */
+Flickity.prototype._unsyncCompanion = function( companion ) {
+  var id = companion.guid;
+  var syncer = this.syncers[ id ];
+  this.off( 'select', syncer.listener );
+  delete this.syncers[ id ];
+};
+
+Flickity.prototype.unsyncAll = function() {
+  for ( var id in this.syncers ) {
+    var syncer = this.syncers[ id ];
+    this._unsync( syncer.flickity );
+  }
+};
+
+// -----  ----- //
+
+return Flickity;
+
+}));
