ef491ff874ebbe5a4c1d6a2602d8a97576f59c32
Adam Heath <adamh@glam.com>
2015-08-25 15:03:01 -0700

    Allow scrolling to not stop the freePlay
    Dragging still stops it

--- js/player.js
+++ js/player.js
@@ -149,7 +149,9 @@ Flickity.prototype._createPlayer = function() {
 
   this.on( 'activate', this.activatePlayer );
   this.on( 'uiChange', this.stopPlayer );
-  this.on( 'pointerDown', this.stopPlayer );
+  this.on( 'pointerDown', this.pausePlayer );
+  this.on( 'pointerUp', this.unpausePlayer );
+  this.on( 'dragStart', this.stopPlayer );
   this.on( 'deactivate', this.deactivatePlayer );
 };
 
@@ -160,6 +162,14 @@ Flickity.prototype.activatePlayer = function() {
   this.player.play();
   eventie.bind( this.element, 'mouseenter', this );
   this.isMouseenterBound = true;
+};
+
+Flickity.prototype.pausePlayer = function() {
+  this.player.pause();
+};
+
+Flickity.prototype.unpausePlayer = function() {
+  this.player.unpause();
 };
 
 Flickity.prototype.stopPlayer = function() {
