0abc3b18e31a48ca2bfce4edc7ffa902da0a99b3
Zack Evans <evans.863@osu.edu>
2016-04-04 15:19:09 -0400

    using gulp-clean-css, gulp-minify-css is deprecated

--- gulpfile.js
+++ gulpfile.js
@@ -146,14 +146,14 @@ gulp.task( 'uglify', [ 'requirejs' ], function() {
 
 // ----- css ----- //
 
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-minify-css');
 
 gulp.task( 'css', function() {
   gulp.src('css/flickity.css')
     // copy to dist
     .pipe( gulp.dest('dist') )
     // minify
-    .pipe( minifyCSS({ advanced: false }) )
+    .pipe( cleanCSS({ advanced: false }) )
     .pipe( rename('flickity.min.css') )
     .pipe( replace( '*/', '*/\n' ) )
     .pipe( gulp.dest('dist') );
