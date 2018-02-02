c884ef596a6e089190b01848b50d914cec83c7f9
Jerry <jones.jeremydavid@gmail.com>
2018-02-02 15:16:06 -0600

    Added tests for sliderTagName option

---
+++ test/unit/slider-tag-name.js
@@ -0,0 +1,25 @@
+QUnit.test( 'sliderTagName', function( assert ) {
+
+  'use strict';
+  var tagNames,
+      elem,
+      flkty,
+      flktySlider;
+
+  tagNames = ['ul','section','figure'];
+
+  for(var i = 0; i < tagNames.length; i++) {
+
+    elem = document.querySelector('#sliderTagName--'+tagNames[i]);
+    flkty = new Flickity( elem, {
+      sliderTagName: tagNames[i]
+    });
+
+    flktySlider = elem.getElementsByClassName('flickity-slider')[0];
+
+    assert.equal( flktySlider.tagName, tagNames[i].toUpperCase(), '.flickity-slider tagName is '+tagNames[i].toUpperCase() );
+  }
+
+  /**/
+
+});
